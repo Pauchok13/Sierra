f35e85cdbaa311855f21068ca968ba0b65309685
Pauchok <79276616+Pauchok13@users.noreply.github.com>
2021-02-22 09:46:39 -0500

    Add files via upload
    More bug fixes.

---
+++ SierraHelpIndex_4.0.js
@@ -0,0 +1,133 @@
+{ name: "help", value: "help command" }
+
+{ name: "help1", value: "general commands" }
+{ name: "findid", value: "id finder" }
+{ name: "guildid", value: "shows the id of the server" }
+{ name: "identify", value: "(ident) shows who the id belongs to" }
+{ name: "sierrainfo", value: "(siei) shows information on the bot" }
+{ name: "invite", value: "(inv) shows the invite link for Sierra" }
+{ name: "listings", value: "shows more information on Sierra" }
+{ name: "poll", value: "creates a poll" }
+{ name: "profile", value: "(pf, me) shows user profile" }
+{ name: "server", value: "DMs a server invite" }
+{ name: "serverinfo", value: "(si) shows all server information" }
+{ name: "info", value: "shows information on a requested item in the shop" }
+
+{ name: "help2", value: "economy commands" }
+{ name: "balance", value: "(bal) shows users balance" }
+{ name: "transfer", value: "(trans) used to transfer deltas to another user" }
+{ name: "buy", value: "used to purchase items and stocks" }
+{ name: "sell", value: "used to sell items back for half price and to sell materials for profit as well as stocks" } 
+{ name: "shop", value: "shows shop directory" }
+{ name: "shop 1", value: "shows power grid items" }
+{ name: "shop 2", value: "shows war items" }
+{ name: "shop 3", value: "shows mine items" }
+{ names: "bank", value: "shows users bank balance" }
+{ name: "xdeltas", value: "shows NoduleWatchstander delta balance" }
+{ name: "xwithdraw", value: "(xwd) withdraws NoduleWatchstander deltas into Origin Sync" }
+{ name: "xwithdrawall", value "(xwdall) withdraws all NoduleWatchstander deltas into Origin Syc" }
+{ name: "deposit", value: "(dep) deposits deltas into users bank balance" }
+{ name: "withdraw", value: "(wd) withdraws deltas from users bank balance into wallet" }
+{ name: "withdrawall", value: "(wdall) withdraws everything from bank into wallet" }
+{ name: "steal", value: "used to attempt to steal from other users wallet" }
+{ name: "salarycollect", value: "collects salary" }
+{ name: "salarylist", value: "shows all salaries" }
+{ name: "donation", value: "(donate) shows Sierra"s next donation goal" } 
+
+{ name: "help3", value: "moderation commands" }
+{ name: "ban", value: "bans a user from the server" }
+{ name: "kick", value: "kicks a user from the server" }
+{ name: "mute", value: "disables a users ability to chat" }
+{ name: "unmute", value: "reenables a users ability to chat" }
+{ name: "help5", value: "premium commands" }
+{ name: "addrole", value: "adds a role to a user" }
+{ name: "announcement", value: "creates a server wide announcement" }
+{ name: "copy", value: "copies and sends a message" }
+{ name: "create" value: "creates a role" }
+{ name: "dm" value: "DMs a user then deletes the message in chat" }
+{ name: "findinvite" value: "(fi) finds invites to a server using a server ID" }
+{ name: "guildowner" value: "(go) shows the server owner" }
+{ name: "listserver" value: "(ls) shows all servers Sierra is in" }
+{ name: "msgdel" value: "deletes messages in chat" }
+{ name: "removerole" value: "removes a role from a user" }
+
+{ name: "help4" value: fun commands" }
+{ name: "chopsticks" value: "chopsticks emoji" }
+{ name: "dice" value: "roles a six sided die" } 
+{ name: "holy" value: "holy hand grenade meme" }
+{ name: "howcute" value: "cuteness on a scale of one through 10" }
+{ name: "ping" value: "Sierra"s response time" }
+{ name: "rps" value: "rock paper scissors" }
+
+{ name: "help6" value: "special commands" }
+{ name: "leaderboard" value: "(lb) leaderboard directory" }
+{ name: "leaderboardsie" value: "(lbsie) SIE leaderboard" }
+{ name: "leaderboardtsc" value: "(lbtsc) TSC leaderboard" }
+{ name: "leaderboardcpa" value: "(lbcpa) CPA leaderboard" }
+{ name: "leaderboardecl" value: "(lbecl) ECL leaderboard" }
+{ name: "leaderboardhse" value: "(lbhse) HSE leaderboard" }
+{ name: "leaderboardexp" value: "(lbexp) exp leaderboard" }
+{ name: "leaderboardxp" value: "(lbxp) xp leaderboard" }
+{ name:" leaderboarddelta" value: "(lbd) delta leaderboard" }
+{ name: "leaderboardbank" value: "(lbb) bank leaderboard" }
+{ name: "lbhp" value: "damage to boss" }
+{ name: "verify" value: verify users" }
+{ name: "type" value: "pokemon type effectiveness and weaknesses" }
+
+{ name: "help7" value: "music commands" }
+{ name: "nowplaying" value: "(np) shows currently playing song" }
+{ name: "play" value: "plays the requested song" }
+{ name: "stop" value: "(dc, disconnect) stops playing the current song" }
+{ name: "queue" value: "(q) shows all songs that have been requested" }
+{ name: "loop" value: "loops the song that is currently being played until disabled by using the  command again" }
+{ name: "shuffle" value: "shuffles the current queue" }
+
+{ name: "help8" value: "Assistance commands" }
+{ name: "mail" value: "DMs <@Nic#5418> about a question or concern. There is also #bug-reports and #suggestions-only on the official Sierra HQ server." }
+{ name: "query" value: "Modmail. Sends a message to the moderators of the server" }
+{ name:"setup" value: "provides instructions on how to setup the muted commands" }
+
+{ name: "help 9" value: "XP/EXP/Experience commands" }
+{ name: "experience" value: "(xp, exp) shows a users experience level" }
+{ name: "experincelist" value: "(xpl, expl) shows all available experience levels" }
+
+{ name: "energy", "value: used to view your power grid assets" 
+{ name: "energycollect", value: "used to collect the energy you produce" }
+{ name: "buy solarpannel", value: "(buy sp) used to buy solar panels. Energy production per hour: 5. Cost: 10 000” }
+{ name: "buy solarstation", value: "(buy ss) used to buy solar stations. Energy production per hour: 25. Cost: 35 000” }
+{ name: "buy solarfarm", value: "(buy sf) used to buy solar farms. Energy production per hour: 100. Cost: 150 000” }
+{ name: "buy solarfusionplant", value: "(buy sfp) used to buy solar fusion plants. Energy production per hour: 250. Cost: 350 000” }
+
+{ name: "mines" value: "(mine) shows the mines that the user owns" }
+{ name: "materials" value: "(mats) shows the mats the user has" }
+{ name: "minecollect" value: "allows you to spend your energy to collect materials" }
+
+{ name: "status" value: "shows your war status. Whether you are hostile or peaceful" }
+{ name: "status hostile" value: "enables hostile mode" }
+{ name: "status peaceful" value: enables peaceful mode" }
+{ name: "forces" value: "DMs you with a non-encrypted version of your forces repot" }
+{ name: "forces public" value: "shows other users in chat you forces publicly" }
+{ name: "attack" value: "currently disabled" }
+{ name: "boss" value: "shows the current community boss" }
+{ name: "bossfight" value: "used to attack the boss from different directions. There is an attack multiplier ranging from 1-10. You can attack from N, E, S, W, NE, NW, SE, SW." }
+
+{ name: "command" value: "sends a full list of command to you in DMs" } 
+
+{ name: "stockhelp" value: "shows a directory of stock commands." }
+{ name: "stocks" value: "shows all stocks that you currently own and their current value." }
+{ name: “infostock” value: "(is) shows information on the requested stock or if you do not request a stock Ticker, you will see a list of stock Tickers to choose from." }
+
+{ name: "hexadeltas", value: "(hexa) Used to view your Hexa-Delta balance. Hexa-Deltas are used to purchase Space Armada Forces." }
+{ name: "armada", value: "Used to view you Space Armada Forces." }
+{ name: "hexamine", value: "Used to manually mine for small amounts of Hexa-Deltas."}
+{ name: "exchange", value: "Used to exchange materials for Hexa-Deltas. E.x s!exchange [amount]" }
+{ name: "buy", value: "Used to purchase Frigates, Destroyers, Battleships, & Enforcers. E.x s!buy [name] [amount]" }
+{ name: "shop 4", value: "Used to view purchasable Space Armada Forces." }
+{ name: "upgrade flagship", value: "Used to upgrade your flagship. Max level is 200." }
+{ name: "upgrade hull”, value: “Used to upgrade your Flagship’s Hull Hp.” }
+{ name: "upgrade shield", value: "Used to upgrade your Flashship’s Shield Durability.” }
+{ name: "upgrade battery", value: "Used to upgrade your Flashship’s Main Weapon Battery.” }
+{ name: "upgrade missile", value: "Used to upgrade your Flashship’s Missile Launcher." }
+{ name: "upgrade deflector", value: "Used to upgrade your Flashship’s Anti-Battery Laser Deflector." }
+{ name: "upgrade drive", value: "Used to upgrade your Flashship’s Anti-Missile Warp Drive." }
+{ name: "upgrades", value: "Used to view how much each Flagship upgrade costs.” }
